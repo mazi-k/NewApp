a1f0923da4c5e45fed6caea2ae354d4367aafd4d
maria <podliza3000@gmail.com>
2022-11-06 13:10:50 +0300

    add ImagesFragment

--- app/src/main/java/com/example/newapp/Constants.kt
+++ app/src/main/java/com/example/newapp/Constants.kt
@@ -1,3 +1,4 @@
 package com.example.newapp
 
 const val GITHUB_USER = "GITHUB_USER"
+const val GALERY_REQEST_CODE = 1
--- app/src/main/java/com/example/newapp/ui/images/ImagesFragment.kt
+++ app/src/main/java/com/example/newapp/ui/images/ImagesFragment.kt
@@ -1,14 +1,27 @@
 package com.example.newapp.ui.images
 
+import android.R
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newapp.App
+import com.example.newapp.GALERY_REQEST_CODE
 import com.example.newapp.core.OnBackPressedListener
 import com.example.newapp.databinding.FragmentImagesBinding
+import moxy.MvpAppCompatActivity
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
+
 
 class ImagesFragment: MvpAppCompatFragment(), ImagesView, OnBackPressedListener {
 
@@ -34,6 +47,37 @@ class ImagesFragment: MvpAppCompatFragment(), ImagesView, OnBackPressedListener 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(viewBinding) {
+            addImageButton.setOnClickListener {
+                addImage()
+            }
+
+            saveImageButton.setOnClickListener {
+                saveImage()
+            }
+        }
+
+
+    }
+
+    private fun addImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALERY_REQEST_CODE)
+    }
+
+
+    private fun saveImage() {
+        val bInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(bInt, GALERY_REQEST_CODE)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == GALERY_REQEST_CODE && resultCode == RESULT_OK){
+            val image = data?.data
+            viewBinding.downloadedImage.setImageURI(image)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
